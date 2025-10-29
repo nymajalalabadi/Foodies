3205977126c18281d960e00ba8dbb957510882fc
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-10-29 09:15:38 -0600

    Added MealsGrid

--- foodies/app/meals/page.tsx
+++ foodies/app/meals/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals, created{''} <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favorite meal and create your own recipe!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
