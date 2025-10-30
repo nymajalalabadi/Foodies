9127483899a4e7337ba2be81126c181d6186dd30
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-10-30 17:04:31 -0600

    Added Error Component

---
+++ foodies/app/meals/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An Error Occurred!</h1>
+            <p>Failed to fetch meals data. Please try again later.</p>
+        </main>
+    );
+}
