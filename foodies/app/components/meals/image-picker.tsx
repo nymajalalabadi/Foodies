f0c5489ac2bb5d4c39a33d500e754a7009e79aa6
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-10-31 11:05:52 -0600

    Updated ImagePicker

--- foodies/app/components/meals/image-picker.tsx
+++ foodies/app/components/meals/image-picker.tsx
@@ -1,21 +1,44 @@
 "use client";
 
 import classes from './image-picker.module.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
 
-export default function ImagePicker({label, name}) {
+export default function ImagePicker({label, name}: {label: string, name: string}) {
+    const [pickedImage, setPickedImage] = useState(null);
 
     const imageInput = useRef(null);
 
     const handleImagePick = () => {
-        imageInput.current.click();
+        imageInput.current?.click();
+    };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     };
 
   return (
     <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
-            <input className={classes.input} type="file" accept="image/png, image/jpeg" id={name} name={name} ref={imageInput} />
+            <div className={classes.preview}>
+                {!pickedImage && <p>Please pick an image.</p>}
+                {pickedImage && <Image src={pickedImage} alt="Picked image" fill />}
+            </div>
+            <input className={classes.input} type="file" accept="image/png, image/jpeg" id={name} name={name} ref={imageInput} onChange={handleImageChange} required />
             <button className={classes.button} type="button" onClick={handleImagePick}>Pick an image</button>
         </div>
     </div>
