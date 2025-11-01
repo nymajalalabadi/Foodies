3bbfb8ce785f6ee08fc5ae38b11fa269f3437af1
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-01 09:39:00 -0600

    Completed function createMeal

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -1,4 +1,8 @@
+import fs from 'node:fs';
+
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -12,6 +16,23 @@ export async function getMeal(slug) {
 }
 
 export async function createMeal(meal) {
-    const result = await db.prepare('INSERT INTO meals (title, slug, image, summary, instructions, creator, creator_email) VALUES (?, ?, ?, ?, ?, ?, ?)').run(meal);
-    return result.lastInsertRowid;
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`;
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (err) => {
+        if (err) {
+            throw new Error('Failed to save image');
+        }
+    });
+
+    meal.image = `/images/${fileName}`;
+
+    db.prepare('INSERT INTO meals (slug, title, image, summary, instructions, creator, creator_email) VALUES (@slug, @title, @image, @summary, @instructions, @creator, @creator_email)').run(meal);
+   
 }
