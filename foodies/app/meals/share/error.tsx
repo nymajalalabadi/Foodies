c41a673c93b91e452e5e83a1fb241da78c238382
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-01 10:11:56 -0600

    Added Error At Share Folder

---
+++ foodies/app/meals/share/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An Error Occurred!</h1>
+            <p>
+                failed to share your meal. Please try again later.
+            </p>
+        </main>
+    );
+}
